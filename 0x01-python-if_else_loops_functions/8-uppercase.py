a8a123aa569e4bb07813a35d4a99f8b3f55f9046
Olutayo0910 <olutayoogunlade0910@gmail.com>
2023-08-08 19:40:23 -0700

    z

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def uppercase(str):
     for c in str:
-        if ord(c) >= ord('a') and ord(b) <= ord('z'):
+        if ord(c) >= ord('a') and ord(c) <= ord('z'):
             char = chr(ord(c) - 32)
         else:
             char = c
