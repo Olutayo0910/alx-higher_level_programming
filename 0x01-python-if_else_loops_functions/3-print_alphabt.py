3ee147e1d054dd4d50937eacf67552ebaa0e6d27
Olutayo0910 <olutayoogunlade0910@gmail.com>
2023-08-08 18:48:04 -0700

    ii

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for ascii_alp in range(ord("a"), ord("z") + 1):
-  if chr(ascii_alp) not in ("q", "e"):
-    print(chr(ascii_alp), end="")
+    if ascii_alp != ord('e') and ascii_alp != ord('q'):
+        print("{:c}".format(ascii_alp), end="")
