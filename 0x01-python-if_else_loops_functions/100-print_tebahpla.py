e9069d791070b76144de394fadbfd71ada02cf45
Olutayo0910 <olutayoogunlade0910@gmail.com>
2023-08-08 20:59:53 -0700

    rm

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
-for ascii_value in range(ord('z'), ord('a') - 1, -1):
-    print(chr(ascii_value).lower() if (ord('z') - \
-    ascii_value) % 2 == 0 else chr(ascii_value).upper(), end='')
+for c in range(ord('z'), ord('a') - 1, -2):
+    print("{:c}{:s}".format(c, chr(c - 33)), end="")
