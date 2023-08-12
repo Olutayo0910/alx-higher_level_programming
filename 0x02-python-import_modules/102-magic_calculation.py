133d8c042d7608324a1062f1512b50b60a899e2a
Olutayo0910 <olutayoogunlade0910@gmail.com>
2023-08-12 07:43:18 -0700

    magic

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,8 +1,6 @@
 #!/usr/bin/pyhton3
-from magic_calulation_102 import add, sub
-
-
 def magic_calculation(a, b):
+    from magic_calulation_102 import add, sub
     if a < b:
         c = add(a, b)
         for i in range(4, 6):
