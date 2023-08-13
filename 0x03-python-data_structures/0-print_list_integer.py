516c9f6fe47dc0a388f65a9ffd7b402af43b2f71
Olutayo0910 <olutayoogunlade0910@gmail.com>
2023-08-12 19:54:43 -0700

    gg

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    def print_list_integer(my_list=[]):
-        for nums in my_list:
-            print("{:d}".format(nums))
+def print_list_integer(my_list=[]):
+    for nums in my_list:
+        print("{:d}".format(nums))
