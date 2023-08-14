5d74d094d008244e141af3d5a993d498b2291352
Olutayo0910 <olutayoogunlade0910@gmail.com>
2023-08-14 15:32:28 -0700

    cpython

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,24 @@
+#include <Python.h>
+
+/**
+ * print_python_list_info - Prints basic info about Python lists.
+ * @p: Pointer to the Python list.
+ */
+void print_python_list_info(PyObject *p)
+{
+	Py_ssize_t size, alloc;
+	Py_ssize_t i;
+	PyObject *item;
+
+	size = PyList_Size(p);
+	alloc = ((PyListObject *)p)->allocated;
+
+	printf("[*] Size of the Python List = %zd\n", size);
+	printf("[*] Allocated = %zd\n", alloc);
+
+	for (i = 0; i < size; i++)
+	{
+		item = PyList_GetItem(p, i);
+		printf("Element %zd: %s\n", i, Py_TYPE(item)->tp_name);
+	}
+}
