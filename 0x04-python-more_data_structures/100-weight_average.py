e1ce1d3ce7f0158b83b5f30770c22187a51bb500
Olutayo0910 <olutayoogunlade0910@gmail.com>
2023-08-16 21:56:49 -0700

    p

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-    if my_list is None or  my_list == []:
+    if my_list is None or my_list == []:
         return 0
     else:
         numerator = sum(score * weight for score, weight in my_list)
