93eebc07915eaa6987e172b559cd9099e14f0ce5
Olutayo0910 <olutayoogunlade0910@gmail.com>
2023-08-30 13:30:08 -0700

    lk

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Define class Square."""
+
+
+class Square:
+    """Create a square."""
+
+    def __init__(self, size):
+        """Initialize a new Square.
+
+        Parameter:
+            (int) size: The size of the new square.
+        """
+        self.__size = size
