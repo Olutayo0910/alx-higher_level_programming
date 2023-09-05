02d8380432d99f6f2795277ea8b8ed8681ce2e50
Olutayo0910 <olutayyoogunlade0910@gmail.com>
2023-09-04 18:11:35 -0700

    task 0

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+# ----------------------------------------------
+# (c) 2023 Olutayo Ogunlade, Lagos, Nigeria
+# www.olutayoogunlade.com.ng
+# olutayoogunlade0910@gmail.com
+# ++++++++++++++++++++++++++++++++++++++++++++++
+"""Rectangle Class module"""
+
+
+class Rectangle:
+    """
+    This class defines a rectangle.
+
+    Attributes:
+        None
+
+    Methods:
+        None
+    """
+    pass
