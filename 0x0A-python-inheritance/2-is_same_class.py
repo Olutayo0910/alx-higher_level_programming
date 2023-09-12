57a37fa49ce48c733caf43068a8cb8841271c744
Olutayo0910 <olutayoogunlade0910@gmail.com>
2023-09-11 20:52:07 -0700

    task 2

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""module for MyList"""
+
+def is_same_class(obj, a_class):
+    """
+    Checks if an object is exactly an instance of the specified class.
+
+    :param obj: The object to check.
+    :param a_class: The class to compare with.
+    :return: True if obj is an instance of a_class, False otherwise.
+    """
+    return type(obj) == a_class
