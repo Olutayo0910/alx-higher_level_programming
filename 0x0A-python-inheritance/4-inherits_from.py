7bfda5f87677f01a6309f12daaad8ea516722473
Olutayo0910 <olutayoogunlade0910@gmail.com>
2023-09-12 02:19:23 -0700

    corr

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -4,7 +4,7 @@
 
 def inherits_from(obj, a_class):
     """
-    Checks  if the object is an instance of a class 
+    Checks  if the object is an instance of a class
     that inherited (directly or indirectly) from the specified class
     :param obj: The object to check.
     :param a_class: The class to compare with.
