21072f1a33a38c44e24aeedf05fd0f79f0877796
Olutayo0910 <olutayoogunlade0910@gmail.com>
2023-09-11 22:12:52 -0700

    task 8

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""module for empty class"""
+
+
+class BaseGeometry:
+    """a class with public instance"""
+
+    def area(self):
+        """
+
+        :return: exception message
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if type(value) is not int:
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
+
+
+class Rectangle(BaseGeometry):
+    def __init__(self, width, height):
+        """initialise width and height"""
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.height = height
