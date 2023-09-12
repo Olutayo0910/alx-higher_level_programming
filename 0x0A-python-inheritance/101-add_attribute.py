cfe078b48d660ab558433e620533e12181799909
Olutayo0910 <olutayoogunlade0910@gmail.com>
2023-09-12 05:47:43 -0700

    corr

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """module for add_attribute"""
 
+
 def add_attribute(obj, attribute, value):
     """function that adds a new attribute to an object if it’s possible:"""
     if hasattr(obj, '__dict__') or \
