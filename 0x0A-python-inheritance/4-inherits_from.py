16eabfba2dd1f2a8eb707684bd82998e121ffee6
Olutayo0910 <olutayoogunlade0910@gmail.com>
2023-09-11 21:26:52 -0700

    task 5

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""module for MyList"""
+
+
+def inherits_from(obj, a_class):
+    """
+    Checks  if the object is an instance of a class 
+    that inherited (directly or indirectly) from the specified class
+    :param obj: The object to check.
+    :param a_class: The class to compare with.
+    :return: True if obj is an instance of a_class, False otherwise.
+    """
+    return issubclass(type(obj), a_class)
