3631ae3195804c0f116afce4b5f0cfc04b35d6d6
Olutayo0910 <olutayoogunlade0910@gmail.com>
2023-09-13 08:41:38 -0700

    task 6

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -6,10 +6,11 @@ import os.path
 
 args = sys.argv[1:]
 
-save_to_json_file = __import__(5-save_to_json_file).save_to_json_file
-load_from_json_file = __import__(6-load_from_json_file).load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 item_list = []
 if os.path.exists("./add_item.json"):
     item_list = load_from_json_file("add_item.json")
+
 save_to_json_file(item_list + args, "add_item.json")
