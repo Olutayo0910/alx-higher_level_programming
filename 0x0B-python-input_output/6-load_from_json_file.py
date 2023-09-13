9ba13ed50cb2b14989b2de08a7b7a2c1f65d8aaf
Olutayo0910 <olutayoogunlade0910@gmail.com>
2023-09-13 07:40:50 -0700

    task 6

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""creates an Object from a JSON file"""
+import json
+
+
+def load_from_json_file(filename):
+    """Create a Python object from a JSON file."""
+    with open(filename, 'r') as fp:
+       return json.load(fp)
