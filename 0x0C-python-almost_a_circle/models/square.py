7b9fec9d1cea7a71840f7c4db97e12f50d5c3e84
Olutayo0910 <olutayoogunlade0910@gmail.com>
2023-09-20 12:58:24 -0700

    square documentation

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -7,7 +7,13 @@ class Square(Rectangle):
     """Square class that inherits from Rectangle"""
 
     def __init__(self, size, x=0, y=0, id=None):
-        """Constructor for Square class."""
+        """Constructor for Square class.
+        Args:
+            size (int): The size of the new Square.
+            x (int): The x coordinate of the new Square.
+            y (int): The y coordinate of the new Square.
+            id (int): The identity of the new Square.
+        """
         super().__init__(size, size, x, y, id)
 
     @property
@@ -27,7 +33,15 @@ class Square(Rectangle):
                                                  self.y, self.width)
 
     def update(self, *args, **kwargs):
-        """Update attributes of the Square."""
+        """Update attributes of the Square.
+        Args:
+            *args (ints): New attribute values.
+                - 1st argument represents id attribute
+                - 2nd argument represents size attribute
+                - 3rd argument represents x attribute
+                - 4th argument represents y attribute
+            **kwargs (dict): New key/value pairs of attributes.
+        """
         if args:
             attrs = ["id", "size", "x", "y"]
             for i, arg in enumerate(args):
@@ -39,6 +53,7 @@ class Square(Rectangle):
                     setattr(self, key, value)
 
     def to_dictionary(self):
+        """dictionary representation of the Square."""
         return {
             "id": self.id,
             "size": self.size,
