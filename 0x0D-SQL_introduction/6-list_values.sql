b7a308082ac9dd0e8d1b088f1ed8459f5da1450f
Olutayo0910 <olutayoogunlade0910@gmail.com>
2023-10-18 21:22:18 -0700

    6 and 7

---
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -0,0 +1,3 @@
+-- List all rows from the first_table in a specific database
+-- Replace with the actual database name
+SELECT * FROM first_table;
---
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -0,0 +1,3 @@
+-- Insert a new row into the first_table in a specific databa
+-- Replace with the actual database name
+INSERT INTO first_table (id, name) VALUES (89, 'Best School');
