57eace43a68cf9ba6d0fd360b9916bbf9d7dba87
Olutayo0910 <olutayoogunlade0910@gmail.com>
2023-11-17 11:01:29 -0800

    task 1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -7,18 +7,17 @@ import MySQLdb
 
 if __name__ == '__main__':
     db = MySQLdb.connect(
-        user=sys.argv[1],
-        passwd=sys.argv[2],
+        username=sys.argv[1],
+        password=sys.argv[2],
         db=sys.argv[3],
         port=3306,
-        host='localhost'
-    )
+        host='localhost')
     cursor = db.cursor()
     query = """
-            SELECT * 
-            FROM states 
-            WHERE CONVERT(`name` USING Latin1) 
-            COLLATE Latin1_General_CS 
+            SELECT *
+            FROM states
+            WHERE CONVERT(`name` USING Latin1)
+            COLLATE Latin1_General_CS
             LIKE 'N%';
             """
     cursor.execute(query)
